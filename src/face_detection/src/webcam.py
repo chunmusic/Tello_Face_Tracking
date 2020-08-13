dccf5faeea082edc4df1089d58238cda37cde209
Pongsakorn <songsuroj.pongsakorn@rm.is.tohoku.ac.jp>
2020-08-13 22:02:17 +0900

    fix on-site bugs

--- src/face_detection/src/webcam.py
+++ src/face_detection/src/webcam.py
@@ -66,9 +66,11 @@ def main():
     width = 320
     height = 240
 
+    i = 0
+
     face_cascade = cv2.CascadeClassifier('/home/pongsakorn/tello_face_tracking/src/face_detection/src/haarcascade_frontalface_default.xml')
 
-    ScaleFactor = 1.2
+    ScaleFactor = 1.5
     minNeighbors = 3
 
     for frame in container.decode(video=0):
@@ -78,12 +80,14 @@ def main():
         gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)        
         faces = face_cascade.detectMultiScale(gray, ScaleFactor, minNeighbors)
 
+        if (i > 900 and i <= 901):
+            takeoff()
 
         if len(faces) == 0:
             print ("No face detected")
             cmd_vel.linear.x = 0.0
             cmd_vel.angular.z = 0.0
-    
+
 
         else:
             for (x,y,w,h) in faces:
@@ -97,7 +101,7 @@ def main():
                 # left_side
                 if center_x > 0 and center_x <= width/3:
                     print("left_side")
-                    cmd_vel.angular.z = -1
+                    cmd_vel.angular.z = -0.4
 
                 # middle_side
                 elif center_x > width/3 and center_x <= 2*(width/3):
@@ -107,30 +111,76 @@ def main():
                 # right_side
                 elif center_x > 2*(width/3) and center_x <= width:
                     print("right_side")
-                    cmd_vel.angular.z = 1
+                    cmd_vel.angular.z = 0.4
+
+
+                # move_forward
+                if w > 0 and w <= width/6:
+                    print("move_forward")
+                    cmd_vel.linear.y = 0.3
+                
+                elif w> width/6 and w <= width/4:
+                    print("no move")
+                    cmd_vel.linear.y = 0
+
+                # move backward 
+                else:
+                    print("move_backward")
+                    cmd_vel.linear.y = -0.3
                                 
+                
                 rospy.loginfo("center x: " + str(x+(w/2)))
                 rospy.loginfo("center y: " + str(y+(h/2)))
+                rospy.loginfo("frame_w: " + str(w))
 
         pub_vel.publish(cmd_vel)
+
+        i = i+1
+        print("iteration: " + str(i))
 
         cv2.imshow('Frame', res)
         cv2.waitKey(1)
 
 
 
+def takeoff():
+    print("taking off")
+    pub_takeoff.publish(takeoff_msg) 
+    rospy.sleep(1)
+    
+    cmd_vel.linear.z = 0.75
+    pub_vel.publish(cmd_vel)
+
+    rospy.sleep(3)
+
+    cmd_vel.linear.z = 0
+    pub_vel.publish(cmd_vel)   
+
+
+
 if __name__ == '__main__':
     
+    rospy.init_node('h264_listener')
+
     pub_takeoff = rospy.Publisher('/tello/takeoff',Empty,queue_size=1)
-    pub_takeoff.publish(Empty()) 
 
     pub_land = rospy.Publisher('/tello/land',Empty,queue_size=1)
 
     pub_vel = rospy.Publisher('/tello/cmd_vel',Twist,queue_size=1)
 
-    cmd_vel = Twist()   
+    takeoff_msg = Empty()
+    cmd_vel = Twist()     
 
     try:
+
+        cmd_vel.linear.z = 0.7
+        pub_vel.publish(cmd_vel)
+        
+        rospy.sleep(3)
+
+        cmd_vel.linear.z = 0
+        pub_vel.publish(cmd_vel)
+
         main()
 
     except BaseException:
