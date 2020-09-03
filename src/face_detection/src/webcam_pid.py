9e0cf83fc9b5ad08a67b4395b48ba063004cb4e5
chunmusic <chun_ps@hotmail.com>
2020-09-03 14:44:56 +0900

    add jupyter notebook

--- src/face_detection/src/webcam_pid.py
+++ src/face_detection/src/webcam_pid.py
@@ -12,6 +12,8 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
 import time
 
+import matplotlib.pyplot as plt
+from scipy.interpolate import BSpline, interp1d
 
 
 class StandaloneVideoStream(object):
@@ -160,12 +162,9 @@ def takeoff():
 
 
 
-
 class PID:
-    """PID Controller
-    """
 
-    def __init__(self, P=0.2, I=0.0, D=0.0, current_time=None):
+    def __init__(self, P=1.0, I=0.8, D=0.001, current_time=None):
 
         self.Kp = P
         self.Ki = I
@@ -178,7 +177,7 @@ class PID:
         self.clear()
 
     def clear(self):
-        """Clears PID computations and coefficients"""
+        
         self.SetPoint = 0.0
 
         self.PTerm = 0.0
@@ -193,13 +192,7 @@ class PID:
         self.output = 0.0
 
     def update(self, feedback_value, current_time=None):
-        """Calculates PID value for given reference feedback
-        .. math::
-            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
-        .. figure:: images/pid_1.png
-           :align:   center
-           Test PID with Kp=1.2, Ki=1, Kd=0.001 (test_pid.py)
-        """
+
         error = self.SetPoint - feedback_value
 
         self.current_time = current_time if current_time is not None else time.time()
@@ -226,35 +219,19 @@ class PID:
             self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
 
     def setKp(self, proportional_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
         self.Kp = proportional_gain
 
     def setKi(self, integral_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
         self.Ki = integral_gain
 
     def setKd(self, derivative_gain):
-        """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
         self.Kd = derivative_gain
 
     def setWindup(self, windup):
-        """Integral windup, also known as integrator windup or reset windup,
-        refers to the situation in a PID feedback controller where
-        a large change in setpoint occurs (say a positive change)
-        and the integral terms accumulates a significant error
-        during the rise (windup), thus overshooting and continuing
-        to increase as this accumulated error is unwound
-        (offset by errors in the other direction).
-        The specific problem is the excess overshooting.
-        """
         self.windup_guard = windup
 
     def setSampleTime(self, sample_time):
-        """PID that should be updated at a regular interval.
-        Based on a pre-determined sampe time, the PID decides if it should compute or return immediately.
-        """
         self.sample_time = sample_time
-
 
 
 
